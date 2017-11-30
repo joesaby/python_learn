2dcbcbd80c43369901ccdce8641c9b3cbac43d01
joseseba <joseseba@cisco.com>
2017-11-30 23:09:16 +0000

    Bubble sort implementation #1

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -11,13 +11,11 @@ class BubbleSort(object):
             for outer_idx, outer_value in enumerate(sorted_list[k:]):
                 left_idx = outer_idx
                 right_idx = outer_idx + 1
-                if right_idx < sub_list_len:
-                    left_val = sorted_list[left_idx]
-                    right_val = sorted_list[right_idx]
-                    if left_val > right_val :
-                        sorted_list[ left_idx] = right_val
-                        sorted_list[ right_idx] = left_val
-                        print sorted_list
+                left_val = sorted_list[left_idx]
+                if right_idx < sub_list_len and left_val > sorted_list[right_idx]:
+                    sorted_list[ left_idx] = sorted_list[right_idx]
+                    sorted_list[ right_idx] = left_val
+                    print sorted_list
 
 sorter = BubbleSort()
-sorter.sort([32,33,15,1,4,6,5,10,5,7,8,9,11,2,12,2])
+sorter.sort_fn([32,33,15,1,4,6,5,10,5,7,8,9,11,2,12,2])
