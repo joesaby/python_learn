5490b2b5a5ac055012cd55ef09c771bd0a688ee3
joseseba <joseseba@cisco.com>
2017-11-30 12:56:37 +0000

    Adding singlelinkedlist

---
+++ linkedlists/single_linkedlist.py
@@ -0,0 +1,125 @@
+import logging
+log = logging.getLogger(__name__)
+
+
+class SingleLinkedList(object):
+    def __init__(self):
+        self._head = None
+        self._tail = None
+
+    def __iter__(self):
+        """
+        Override default iterator with a generator
+        :return:
+        """
+        current_node = self._head
+        while current_node:
+            yield current_node
+            current_node = current_node.get_next_node()
+
+    def add_to_head(self, data):
+        node = Node(data)
+        if self._head:
+            # Set the new node's next node to be current head
+            # If only one node exists head & tail are same, so no update required to tail
+            node.set_next_node(self._head)
+        else:
+            # Means tail doesnt exist too so set the tail
+            self._tail = node
+        self._head = node
+
+    def add(self, data):
+        """
+        Simply adds to the tail
+        :param data:
+        :return:
+        """
+        node = Node(data)
+        if self._tail:
+            # Set the current tails next node to be the new node
+            # If there was ever only one node, head & tail are same
+            self._tail.set_next_node(node)
+        else:
+            # Means head also doesnt exist
+            self._head = node
+        self._tail = node
+
+    def delete_node(self, key):
+        previous_node = None
+        for node in self :
+            if node.get_data() == key:
+                pass
+
+    def delete_head(self):
+        if self._head:
+            self._head = self._head.get_next_node()
+        # After resetting the current head, if head is null (ie if head
+        # and tail were same), then set tail to null as well
+        if not self._head:
+            self._tail = None
+
+    def delete_tail(self):
+        if self._tail == self._head:
+            self._tail = None
+            self._head = None
+            return
+
+        second_last_node = None
+        # At this stage you are guaranteed to have a list
+        # of more that one element
+        count = 0
+        for node in self :
+            # Traverse till the last node
+            if not node.get_next_node():
+                self._tail = second_last_node
+                # Empty out next node
+                self._tail.set_next_node()
+                # If count of list is 1 then set the
+                # head to be tail
+                if count == 1:
+                    self._head == self._tail
+                return self._tail
+            second_last_node = node
+            count +=1
+
+    def add_before(self, data, key):
+        for node in self:
+            pass
+
+    def add_after(self, data, key):
+        raise NotImplemented
+
+    def get_head(self):
+        return self._head
+
+    def get_tail(self):
+        return self._tail
+
+class Node(object):
+
+    def __init__(self, data, next_node = None):
+        self._data = data
+        self._next_node = next_node
+
+    def get_next_node(self):
+        return self._next_node
+
+    def am_i_last(self):
+        return True if self._next_node is None else False
+
+    def set_next_node(self, node = None):
+        prev_node = self._next_node
+        self._next_node = node
+        log.debug("I am {}, My next node is {}".format(self._data,
+                                                       self._next_node.get_data() if self._next_node else None))
+        return prev_node
+
+    def get_data(self):
+        return self._data
+
+    def __eq__(self, node):
+        assert (isinstance(node, Node))
+        return self._data == node.get_data()
+
+    def __str__(self):
+        return self._data
---
+++ test/test_single_linkedlist.py
@@ -0,0 +1,58 @@
+import unittest
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+from linkedlists.single_linkedlist import SingleLinkedList , Node
+
+DATA = ["abcd", "defg", "hijk", "lmno"]
+
+class SingleLinkedListTests(unittest.TestCase):
+
+    def test_add_to_head(self):
+        ret_list = []
+        ll = SingleLinkedList()
+        for el in DATA:
+            ll.add_to_head(el)
+            self.assertTrue(ll.get_head() == Node(el))
+        for retdata in ll:
+            ret_list.append(retdata.get_data())
+        self.assertEquals(ret_list[::-1], DATA, "Expect return list to be a reverse of added list")
+
+    def test_add_to_tail(self):
+        ret_list = []
+        ll = SingleLinkedList()
+        for el in DATA:
+            ll.add(el)
+            self.assertTrue(ll.get_tail() == Node(el))
+        for retdata in ll:
+            ret_list.append(retdata.get_data())
+        self.assertEquals(ret_list, DATA, "Expect return list to be the same as added list")
+
+    def test_delete_tail(self):
+        ll = SingleLinkedList()
+        # Only one node where Test tail & head are same
+        ll.add("some")
+        self.assertEquals(ll.get_head().get_data(), "some")
+        self.assertEquals(ll.get_tail().get_data(), "some")
+        ll.delete_tail()
+        self.assertEquals(ll.get_head(), None)
+        self.assertEquals(ll.get_tail(), None)
+
+    def test_delete_tail_list_contains_2_items(self):
+        ll = SingleLinkedList()
+        for el in DATA[:2]:
+            ll.add(el)
+        ll.delete_tail()
+        self.assertEquals(ll.get_head().get_data(), DATA[0])
+        self.assertEquals(ll.get_tail().get_data(), DATA[0])
+
+    def test_delete_tail_list_contains_more_than_2_items(self):
+        ll = SingleLinkedList()
+        for el in DATA:
+            ll.add(el)
+
+        ll.delete_tail()
+        log.debug("Current Tail {}".format(ll.get_tail().get_data()))
+        self.assertEquals(ll.get_head().get_data(), DATA[0])
+        self.assertEquals(ll.get_tail().get_data(), DATA[len(DATA) - 2])
